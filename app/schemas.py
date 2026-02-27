37af98c0e653de0a3ac90bfd4c1858f3948ecc7f
Fadhil Muhammed N C <ncfadhil@gmail.com>
2026-02-28 00:42:45 +0530

    feat(schema): created schema for request and response validation

---
+++ app/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class EvaluationRequest(BaseModel):
+    model: str
+    prompt: str
+    
+class EvaluationResponse(BaseModel):
+    model: str
+    latency_ms: float
+    similarity_score: float
+    resposen: str
