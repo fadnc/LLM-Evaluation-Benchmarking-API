0dc65d784fe07de1f7585e25e5aab4996d895f02
Fadhil Muhammed N C <ncfadhil@gmail.com>
2026-02-28 22:46:37 +0530

    load sentence transformer rather than at only importing

--- app/eval.py
+++ app/eval.py
@@ -4,7 +4,18 @@ import numpy as np
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
+
+_model = None
+
+def get_embeddings():
+    global _model
+    if _model is None:
+        _model =  SentenceTransformer('all-MiniLM-L6-v2')
+    return _model
+    
+
 def compute_similarity(prompt: str, response:str) -> float:
+    model = get_embeddings()
     embeddings = model.encode([prompt, response])
     similarity = cosine_similarity([embeddings[0]], [embeddings[1]])[0][0]
     return float(similarity)
