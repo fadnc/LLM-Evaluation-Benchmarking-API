7be29c2b8786a2707f3d32d72000a6e2a54d9e03
Fadhil Muhammed N C <ncfadhil@gmail.com>
2026-03-06 16:22:03 +0530

    Update: Changed anthropic model

--- app/models/claude.py
+++ app/models/claude.py
@@ -10,7 +10,7 @@ def claude_generate(prompt: str):
     start = time.time()
 
     response = client.messages.create(
-        model="claude-3-haiku-20240307",
+        model="claude-haiku-4-5-20251001",
         max_tokens=500,
         messages=[{
             "role": "user",
